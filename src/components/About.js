271b6256461f273bcecdb2553948cb7f5a81e103
Mark Prettyman <markiesmyname@Marks-iMac.lan>
2023-09-06 12:36:13 -0700

    react mini project

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About({ image, about }) {
+    const imageUrl = image || "https://via.placeholder.com/215";
+
+    return (
+        <aside>
+            <img src={imageUrl} alt="blog logo" />
+            <p>{about}</p>
+        </aside>
+    );
+}
+
+export default About;
